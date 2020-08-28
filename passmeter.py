0d8a83a4f4d1c494a6e2988a11df3651afb32fe2
Kevin Cui <krazycavin@gmail.com>
2020-08-28 14:50:23 +0200

    Code refactoring

--- passmeter.py
+++ passmeter.py
@@ -17,18 +17,194 @@ def parseArgs():
     return parser.parse_args()
 
 
-def calcResult(score):
+def getPassword(arguments):
+    if arguments.password:
+        return arguments.password[0]
+    else:
+        try:
+            return getpass()
+        except KeyboardInterrupt:
+            sys.exit(1)
+
+
+def calcScore(dict):
+    for k in dict.keys():
+        if dict[k]["count"] > 0:
+            if dict[k]["score"] == 0:
+                dict[k]["score"] = dict[k]["count"] * dict[k]["mult"]
+
+
+def printTable(dict):
+    table = Table()
+    table.add_column("", style="cyan")
+    table.add_column("Rate", style="white")
+    table.add_column("Count", justify="right", style="yellow")
+    table.add_column("Bonus", justify="right", style="green")
+    for k in dict.keys():
+        table.add_row(dict[k]["text"], dict[k]["rate"], str(dict[k]["count"]), setBonuscolor(dict[k]['score']))
+    console = Console()
+    console.print(table)
+
+
+def printResult(dict, scoreOnly):
+    score = 0
+    complexity = "unknown"
+    for k in dict.keys():
+        score += dict[k]["score"]
+
     if score < 20:
-        return str(max(score, 0)) + "%", "Very Weak"
+        score = max(0, score)
+        complexity = "Very Weak"
     if score >= 20 and score < 40:
-        return str(score) + "%", "Weak"
+        complexity = "Weak"
     if score >= 40 and score < 60:
-        return str(score) + "%", "Good"
+        complexity = "Good"
     if score >= 60 and score < 80:
-        return str(score) + "%", "Strong"
+        complexity = "Strong"
     if score >= 80:
-        return str(min(score, 100)) + "%", "Very Strong"
-    return 'n/a', 'Unknown'
+        score = min(score, 100)
+        complexity = "Very Strong"
+
+    if scoreOnly:
+        print(score)
+    else:
+        print(complexity)
+        print("Score: " + str(score) + "%")
+        print("Complexity: " + complexity)
+        printTable(dict)
+
+
+def countAlphaUC(dict, password):
+    tmp = ""
+    arrPwd = list(password)
+    arrPwdLen = len(arrPwd)
+    for a in range(0, arrPwdLen):
+        if re.match(r'[A-Z]', arrPwd[a]):
+            if tmp != "":
+                if (tmp + 1) == a:
+                    dict["consecAlphaUC"]["count"] += 1
+            tmp = a
+            dict["alphaUC"]["count"] += 1
+
+    if dict["alphaUC"]["count"] > 0 \
+            and dict["alphaUC"]["count"] < dict["length"]["count"]:
+        dict["alphaUC"]["mult"] = 2
+        dict["alphaUC"]["score"] = (dict["length"]["count"] - dict["alphaUC"]["count"]) * dict["alphaUC"]["mult"]
+
+
+def countAlphaLC(dict, password):
+    tmp = ""
+    arrPwd = list(password)
+    arrPwdLen = len(arrPwd)
+    for a in range(0, arrPwdLen):
+        if re.match(r'[a-z]', arrPwd[a]):
+            if tmp != "":
+                if (tmp + 1) == a:
+                    dict["consecAlphaLC"]["count"] += 1
+            tmp = a
+            dict["alphaLC"]["count"] += 1
+
+    if dict["alphaLC"]["count"] > 0 \
+            and dict["alphaLC"]["count"] < dict["length"]["count"]:
+        dict["alphaLC"]["mult"] = 2
+        dict["alphaLC"]["score"] = (dict["length"]["count"] - dict["alphaLC"]["count"]) * dict["alphaLC"]["mult"]
+
+
+def countNumber(dict, password):
+    tmp = ""
+    arrPwd = list(password)
+    arrPwdLen = len(arrPwd)
+    for a in range(0, arrPwdLen):
+        if re.match(r'[0-9]', arrPwd[a]):
+            if a > 0 and a < (arrPwdLen - 1):
+                dict["midChar"]["count"] += 1
+            if tmp != "":
+                if (tmp + 1) == a:
+                    dict["consecNumber"]["count"] += 1
+            tmp = a
+            dict["number"]["count"] += 1
+
+
+def countSymbol(dict, password):
+    arrPwd = list(password)
+    arrPwdLen = len(arrPwd)
+    for a in range(0, arrPwdLen):
+        if re.match(r'[^a-zA-Z0-9_]', arrPwd[a]):
+            if a > 0 and a < (arrPwdLen - 1):
+                dict["midChar"]["count"] += 1
+            dict["symbol"]["count"] += 1
+
+
+def countRepChar(dict, password):
+    repInc = 0
+    arrPwd = list(password)
+    arrPwdLen = len(arrPwd)
+    for a in range(0, arrPwdLen):
+        charExists = False
+        for b in range(0, arrPwdLen):
+            if arrPwd[a] == arrPwd[b] and a != b:
+                charExists = True
+                repInc += abs(arrPwdLen/(b-a))
+        if charExists:
+            dict["repChar"]["count"] += 1
+            nUnqChar = arrPwdLen - dict["repChar"]["count"]
+            repInc = math.ceil(repInc/nUnqChar) if nUnqChar else math.ceil(repInc)
+    dict["repChar"]["score"] = repInc * dict["repChar"]["mult"]
+
+
+def countSeqAlpha(dict, password):
+    alphas = "abcdefghijklmnopqrstuvwxyz"
+    for s in range(0, 24):
+        fwd = alphas[s:s+3]
+        rev = fwd[::-1]
+        if password.lower().find(fwd) != -1 \
+                or password.lower().find(rev) != -1:
+            dict["seqAlpha"]["count"] += 1
+
+
+def countSeqNumber(dict, password):
+    numerics = "01234567890"
+    for s in range(0, 9):
+        fwd = numerics[s:s+3]
+        rev = fwd[::-1]
+        if password.lower().find(fwd) != -1 \
+                or password.lower().find(rev) != -1:
+            dict["seqNumber"]["count"] += 1
+
+
+def countSeqSymbol(dict, password):
+    symbols = ")!@#$%^&*()"
+    for s in range(0, 9):
+        fwd = symbols[s:s+3]
+        rev = fwd[::-1]
+        if password.lower().find(fwd) != -1 \
+                or password.lower().find(rev) != -1:
+            dict["seqSymbol"]["count"] += 1
+
+
+def countAlphasOnly(dict):
+    if max(dict["alphaLC"]["count"], dict["alphaUC"]["count"]) > 0 \
+            and max(dict["symbol"]["count"], dict["number"]["count"]) == 0:
+        dict["alphasOnly"]["count"] = dict["length"]["count"]
+
+
+def countNumbersOnly(dict):
+    if max(dict["alphaLC"]["count"], dict["alphaUC"]["count"], dict["symbol"]["count"]) == 0 \
+            and dict["number"]["count"] > 0:
+        dict["numbersOnly"]["count"] = dict["length"]["count"]
+
+
+def countRequirements(dict):
+    minReqChars = 4
+    minPwdLen = 8
+    arrChars = ["alphaUC", "alphaLC", "number", "symbol"]
+    for c in arrChars:
+        if dict[c]["count"] > 0:
+            dict["requirements"]["count"] += 1
+    if dict["length"]["count"] >= minPwdLen:
+        dict["requirements"]["count"] += 1
+        if dict["requirements"]["count"] >= minReqChars:
+            dict["requirements"]["mult"] = 2
 
 
 def setBonuscolor(score):
@@ -40,18 +216,11 @@ def setBonuscolor(score):
 
 
 def main():
-    args = parseArgs()
-
-    nTmpAlphaUC = nTmpAlphaLC = nTmpNumber = ""
-    sAlphas = "abcdefghijklmnopqrstuvwxyz"
-    sNumerics = "01234567890"
-    sSymbols = ")!@#$%^&*()"
-    nMinPwdLen = 8
-    nMinReqChars = 4
+    pwd = getPassword(parseArgs())
     dictRule = {
         "length": {"count": 0, "mult": 4, "score": 0, "text": "Number of Characters", "rate": "+(n*4)"},
-        "alphaUC": {"count": 0, "mult": 2, "score": 0, "text": "Uppercase Letters", "rate": "+((len-n)*2)"},
-        "alphaLC": {"count": 0, "mult": 2, "score": 0, "text": "Lowercase Letters", "rate": "+((len-n)*2)"},
+        "alphaUC": {"count": 0, "mult": 0, "score": 0, "text": "Uppercase Letters", "rate": "+((len-n)*2)"},
+        "alphaLC": {"count": 0, "mult": 0, "score": 0, "text": "Lowercase Letters", "rate": "+((len-n)*2)"},
         "number": {"count": 0, "mult": 4, "score": 0, "text": "Numbers", "rate": "+(n*4)"},
         "symbol": {"count": 0, "mult": 6, "score": 0, "text": "Symbols", "rate": "+(n*6)"},
         "midChar": {"count": 0, "mult": 2, "score": 0, "text": "Middle Numbers of Symbols", "rate": "+(n*2)"},
@@ -66,126 +235,46 @@ def main():
         "seqNumber": {"count": 0, "mult": -3, "score": 0, "text": "Sequential Numbers (3+)", "rate": "-(n*3)"},
         "seqSymbol": {"count": 0, "mult": -3, "score": 0, "text": "Sequential Symbols (3+)", "rate": "-(n*3)"}
     }
-    if args.password:
-        pwd = args.password[0]
-    else:
-        try:
-            pwd = getpass()
-        except KeyboardInterrupt:
-            sys.exit(1)
+
+    # Count password length
     dictRule["length"]["count"] = len(pwd)
-    arrPwd = list(pwd)
-    arrPwdLen = len(arrPwd)
-    score = 0
 
-    # Check Uppercase, Lowercase, Numeric and Symbol
-    nRepInc = 0
-    for a in range(0, arrPwdLen):
-        if re.match(r'[A-Z]', arrPwd[a]):
-            if nTmpAlphaUC != "":
-                if (nTmpAlphaUC + 1) == a:
-                    dictRule["consecAlphaUC"]["count"] += 1
-            nTmpAlphaUC = a
-            dictRule["alphaUC"]["count"] += 1
-        elif re.match(r'[a-z]', arrPwd[a]):
-            if nTmpAlphaLC != "":
-                if (nTmpAlphaLC + 1) == a:
-                    dictRule["consecAlphaLC"]["count"] += 1
-            nTmpAlphaLC = a
-            dictRule["alphaLC"]["count"] += 1
-        elif re.match(r'[0-9]', arrPwd[a]):
-            if a > 0 and a < (arrPwdLen - 1):
-                dictRule["midChar"]["count"] += 1
-            if nTmpNumber != "":
-                if (nTmpNumber + 1) == a:
-                    dictRule["consecNumber"]["count"] += 1
-            nTmpNumber = a
-            dictRule["number"]["count"] += 1
-        elif re.match(r'[^a-zA-Z0-9_]', arrPwd[a]):
-            if a > 0 and a < (arrPwdLen - 1):
-                dictRule["midChar"]["count"] += 1
-            dictRule["symbol"]["count"] += 1
+    # Check Uppercase
+    countAlphaUC(dictRule, pwd)
 
-        # Check for repeat characters
-        bCharExists = False
-        for b in range(0, arrPwdLen):
-            if arrPwd[a] == arrPwd[b] and a != b:
-                bCharExists = True
-                nRepInc += abs(arrPwdLen/(b-a))
+    # Count Lowercase
+    countAlphaLC(dictRule, pwd)
 
-        if bCharExists:
-            dictRule["repChar"]["count"] += 1
-            nUnqChar = arrPwdLen - dictRule["repChar"]["count"]
-            nRepInc = math.ceil(nRepInc/nUnqChar) if nUnqChar else math.ceil(nRepInc)
-    dictRule["repChar"]["score"] = nRepInc * dictRule["repChar"]["mult"]
+    # Count Numeric
+    countNumber(dictRule, pwd)
 
-    if dictRule["alphaLC"]["count"] > 0 and dictRule["alphaLC"]["count"] < dictRule["length"]["count"]:
-        dictRule["alphaLC"]["score"] = (dictRule["length"]["count"] - dictRule["alphaLC"]["count"]) * dictRule["alphaLC"]["mult"]
-    if dictRule["alphaUC"]["count"] > 0 and dictRule["alphaUC"]["count"] < dictRule["length"]["count"]:
-        dictRule["alphaUC"]["score"] = (dictRule["length"]["count"] - dictRule["alphaUC"]["count"]) * dictRule["alphaUC"]["mult"]
+    # Count Symbol
+    countSymbol(dictRule, pwd)
 
-    # Check for sequential alpha string patterns (forward and reverse)
-    for s in range(0, 24):
-        sFwd = sAlphas[s:s+3]
-        sRev = sFwd[::-1]
-        if pwd.lower().find(sFwd) != -1 or pwd.lower().find(sRev) != -1:
-            dictRule["seqAlpha"]["count"] += 1
+    # Count requirements
+    countRequirements(dictRule)
 
-    # Check for sequential numeric string patterns (forward and reverse)
-    for s in range(0, 9):
-        sFwd = sNumerics[s:s+3]
-        sRev = sFwd[::-1]
-        if pwd.lower().find(sFwd) != -1 or pwd.lower().find(sRev) != -1:
-            dictRule["seqNumber"]["count"] += 1
+    # Count deduction points of letters only
+    countAlphasOnly(dictRule)
 
-    # Check for sequential symbol string patterns (forward and reverse)
-    for s in range(0, 9):
-        sFwd = sSymbols[s:s+3]
-        sRev = sFwd[::-1]
-        if pwd.lower().find(sFwd) != -1 or pwd.lower().find(sRev) != -1:
-            dictRule["seqSymbol"]["count"] += 1
+    # Count deduction points of numbers only
+    countNumbersOnly(dictRule)
 
-    # Point deductions
-    if max(dictRule["alphaLC"]["count"], dictRule["alphaUC"]["count"]) > 0 and max(dictRule["symbol"]["count"], dictRule["number"]["count"]) == 0:
-        dictRule["alphasOnly"]["count"] = dictRule["length"]["count"]
+    # Count deduction points of repeat characters
+    countRepChar(dictRule, pwd)
 
-    if max(dictRule["alphaLC"]["count"], dictRule["alphaUC"]["count"], dictRule["symbol"]["count"]) == 0 and dictRule["number"]["count"] > 0:
-        dictRule["numbersOnly"]["count"] = dictRule["length"]["count"]
+    # Count deduction points of sequential letters (forward and reverse)
+    countSeqAlpha(dictRule, pwd)
 
-    # Determine if mandatory requirements have been met
-    arrChars = ["alphaUC", "alphaLC", "number", "symbol"]
-    for c in arrChars:
-        if dictRule[c]["count"] > 0:
-            dictRule["requirements"]["count"] += 1
+    # Count deduction points of sequential numbers (forward and reverse)
+    countSeqNumber(dictRule, pwd)
 
-    if dictRule["length"]["count"] >= nMinPwdLen:
-        dictRule["requirements"]["count"] += 1
-        if dictRule["requirements"]["count"] >= nMinReqChars:
-            dictRule["requirements"]["mult"] = 2
+    # Count deduction points of sequential symbols (forward and reverse)
+    countSeqSymbol(dictRule, pwd)
 
-    for k in dictRule.keys():
-        if dictRule[k]["count"] > 0:
-            if dictRule[k]["score"] == 0:
-                dictRule[k]["score"] = dictRule[k]["count"] * dictRule[k]["mult"]
-            score += dictRule[k]["score"]
-
-    fScore, fComplexity = calcResult(score)
-    if args.score_only:
-        print(fScore)
-    else:
-        print("Score: " + fScore)
-        print("Complexity: " + fComplexity)
-
-        table = Table()
-        table.add_column("", style="cyan")
-        table.add_column("Rate", style="white")
-        table.add_column("Count", justify="right", style="yellow")
-        table.add_column("Bonus", justify="right", style="green")
-        for k in dictRule.keys():
-            table.add_row(dictRule[k]["text"], dictRule[k]["rate"], str(dictRule[k]["count"]), setBonuscolor(dictRule[k]['score']))
-
-        console = Console()
-        console.print(table)
+    # Calculate final score
+    calcScore(dictRule)
+    printResult(dictRule, parseArgs().score_only)
 
 
 if __name__ == '__main__':
